838442ceec9ef36dec4ba887faed1ae9c390099b
attilax <1466519819@qq.com>
2014-11-22 22:39:52 +0800

    ob7

--- AtiInterpreter/src/com/attilax/designpatter/parterr/t.java
+++ AtiInterpreter/src/com/attilax/designpatter/parterr/t.java
@@ -2,71 +2,72 @@ package com.attilax.designpatter.parterr;
 
 import java.util.List;
 
+import com.attilax.frontchk.IFrontCheck;
 import com.attilax.json.JsonUtil4jackjson;
 import com.attilax.review.AlreadyReviewPassedEx;
 import com.attilax.review.IReview;
+import com.attilax.review.ReviewBackEx;
 import com.attilax.review.ReviewX;
 
 public class t implements IReview {
-	
-	public t()
-	{
-		this.ReltDataCheckX1=new IReltDataCheckX() {
+
+	public t() {
+		this.ReltDataCheckX1 = new IReltDataCheckX() {
 
 			@Override
 			public void checkRelt_shouldBeNoRelt(int i) throws HasReltDataEx {
 				if (i == 5)
 					throw new HasReltDataEx("HasReltDataEx");
-				
+
 			}
 		};
-//		this.revChkX=new IReviewCheckX() {
-//			
-//			@Override
-//			public void reviewPassedCheck_shouldBeYetNoReviewOrBack(int i) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		};
+		// this.revChkX=new IReviewCheckX() {
+		//
+		// @Override
+		// public void reviewPassedCheck_shouldBeYetNoReviewOrBack(int i) {
+		// // TODO Auto-generated method stub
+		//
+		// }
+		// };
 	}
-   
-	IReltDataCheckX  ReltDataCheckX1;
-//	IReviewCheckX   revChkX;
+
+	IReltDataCheckX ReltDataCheckX1;
+
+	// IReviewCheckX revChkX;
 	public static void main(String[] args) {
 		System.out.println(new t().multiDel_byDwr());
 	}
 
-	private String multiDel_byDwr()
-	{
-	try {
-		return	multiDel();
-	} catch (PartProcessErrEx e) {
-		e.setTypex("PartProcessErrEx");
-		return toJsonStr( e);
+	private String multiDel_byDwr() {
+		try {
+			return multiDel();
+		} catch (PartProcessErrEx e) {
+			e.setTypex("PartProcessErrEx");
+			return toJsonStr(e);
+		}
+		// return toJsonStr(new PartProcessErrEx(pex.li, "PartProcessErrEx"));
+
 	}
-	//	return toJsonStr(new PartProcessErrEx(pex.li, "PartProcessErrEx"));
-	
-	
-	}
-	
+
 	private String multiDel() throws PartProcessErrEx {
 		PartErrX pex = new PartErrX();
 		for (int i = 0; i < 10; i++) {
 			try {
 				del(i);
-			 
+
 			} catch (Exception e) {
 				ErrorItem ei = new ErrorItem();
 				ei.id = String.valueOf(i);
 				ei.msg = e.getMessage();
 				pex.add(ei);
-				 
+
 			}
 		}
 		if (pex.li.size() == 0)
 			return "ok";
 
-		throw  (new PartProcessErrEx( JsonUtil4jackjson.buildNormalBinder().toJson(pex.li) ));
+		throw (new PartProcessErrEx(JsonUtil4jackjson.buildNormalBinder()
+				.toJson(pex.li)));
 
 	}
 
@@ -75,28 +76,45 @@ public class t implements IReview {
 		return JsonUtil4jackjson.buildNormalBinder().toJson(obj);
 	}
 
-	private void del(int i) throws HasReltDataEx, AlreadyReviewPassedEx {
+	IFrontCheck frtchk;
+
+	private void del(int i) throws HasReltDataEx, AlreadyReviewPassedEx,Exception {
+		if (frtchk != null) //default can b use front chkx...only exist chkor ,then use chkor.. todox ob7
+			frtchk.execute(i);
 		ReviewX.reviewPassedCheck_shouldBeYetNoReviewOrBack(i);
-		ReltDataCheckX1.checkRelt_shouldBeNoRelt(i);
-		
+		if (ReltDataCheckX1 != null)
+			ReltDataCheckX1.checkRelt_shouldBeNoRelt(i);
+
 		if (i == 8)
 			throw new RuntimeException("unkk");
-		 
-			
 
 	}
-	
-	public void editAftSave(int i)
-	{
-		
+
+	public void editAftSave(int i) {
+
 	}
+ /**
+  * 4dwr
+  * @param obj
+  * @return
+  */
+	public boolean reviewPassedCheck_dwr(Object obj)   {
+		try {
+			ReviewX.reviewPassedCheck_shouldBeYetNoReviewOrBack((int) obj);
+		} catch (AlreadyReviewPassedEx e) {
+			return true;
+		}
+		return false;
+	}
+
+ 
 
 	@Override
-	public boolean reviewPassed(Object obj) {
+	public boolean reviewPassedCheck(Object obj) throws AlreadyReviewPassedEx {
+		ReviewX.reviewPassedCheck_shouldBeYetNoReviewOrBack((int) obj);
 		return false;
-//		int id=(int) obj;
-//		if (i == 5)
-//			throw new 
 	}
+
+	 
 
 }
--- AtiInterpreter/src/com/attilax/review/IReview.java
+++ AtiInterpreter/src/com/attilax/review/IReview.java
@@ -2,6 +2,12 @@ package com.attilax.review;
 
 public interface IReview {
 	
-	boolean reviewPassed(Object obj);
+	/**
+	 * 4 edit ,del
+	 * @param obj
+	 * @return
+	 * @throws AlreadyReviewPassedEx,ReviewBackEx 
+	 */
+	boolean reviewPassedCheck(Object obj) throws AlreadyReviewPassedEx,ReviewBackEx;
 
 }
